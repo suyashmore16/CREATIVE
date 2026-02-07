721ca2015ce81f4b565a050d644fd14fb326d789
suyashmore16 <suyash.more@gmail.com>
2026-02-07 13:56:21 -0500

    Implement EEG feature extraction in CREATIVEmain.py
    Added feature extraction using extract_features function.

--- CREATIVEmain.py
+++ CREATIVEmain.py
@@ -1,5 +1,20 @@
 import mne
 from eeg_preprocessing import preprocess_eeg
+import numpy as np
+from eeg_features import extract_features
+import joblib
 
+#preprocessing code
 raw = mne.io.read_raw_edf("subject01.edf", preload=True)
 epochs, ica = preprocess_eeg(raw)
+
+#feature extraction main
+sfreq = 256
+
+# example EEG window: (channels, samples)
+epoch = np.random.randn(4, 512)
+
+features = extract_features(epoch, sfreq)
+
+print(features)
+print(features.shape)
