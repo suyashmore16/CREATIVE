574a73046a589c588abb3401e5948414bc66f42e
suyashmore16 <suyash.more@gmail.com>
2026-02-14 13:03:48 -0500

    Add additional imports for EEG processing

--- CREATIVEmain.py
+++ CREATIVEmain.py
@@ -3,6 +3,10 @@ from eeg_preprocessing import preprocess_eeg
 import numpy as np
 from eeg_features import extract_features
 import joblib
+from LSL import LSLEEGReader
+from raw_logger import RawEEGLogger
+from event_logger import EventLogger
+from session_metadata import write_session_metadata
 
 #preprocessing code
 raw = mne.io.read_raw_edf("subject01.edf", preload=True)
@@ -18,3 +22,4 @@ features = extract_features(epoch, sfreq)
 
 print(features)
 print(features.shape)
+
